78fdf3263d71eba86001c22f96187e71cae4f746
andeo <andy.ontaneda@gmail.com>
2017-12-09 11:07:41 -0500

    file making

---
+++ assets/js/firebase.js
@@ -0,0 +1,12 @@
+<script>
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyCSCCC6_58bJBzPJBRxbJ05sdtJPblwmBA",
+    authDomain: "fir-54682.firebaseapp.com",
+    databaseURL: "https://fir-54682.firebaseio.com",
+    projectId: "fir-54682",
+    storageBucket: "",
+    messagingSenderId: "489038949997"
+  };
+  firebase.initializeApp(config);
+</script>
